dcd3a4ff2f03213a6b79963557f551edebcbaa7d
MhZ9191 <hegic91@gmail.com>
2026-03-16 16:25:50 +0100

    Add public folder

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = parseInt(process.env.APP_PORT);
 const appUrl = "http://localhost:" + port;
 const movieRouter = require("./routers/movieRouter");
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use("/movies", movieRouter);
 
