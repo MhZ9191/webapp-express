65dc96b9bb354b73fadeaf6fb90121986be660f8
MhZ9191 <hegic91@gmail.com>
2026-03-17 13:46:08 +0100

    Add cors

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const port = parseInt(process.env.APP_PORT);
 const appUrl = "http://localhost:" + port;
 const movieRouter = require("./routers/movieRouter");
 const error = require("./middlewares/errorHandler");
+const cors = require("cors");
 
+app.use(cors({ origin: process.env.FRONT_URL }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use("/movies", movieRouter);
